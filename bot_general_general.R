0f3b451a34ca2222cb7735fa94fe43707b2afe22
Matan <matanhakim@gmail.com>
2022-10-17 20:15:13 +0300

    Adds GitHub Actions YAML file and rtweet code. For now, it is supposed to tweet every 10 minutes.

--- bot_general_general.R
+++ bot_general_general.R
@@ -1,5 +1,14 @@
 # Load libraries
 library(tidyverse)
+library(rtweet)
+
+# Create Twitter token
+general_general_bot_token <- rtweet_bot(
+  api_key       = Sys.getenv("TWITTER_CONSUMER_API_KEY"),
+  api_secret    = Sys.getenv("TWITTER_CONSUMER_API_SECRET"),
+  access_token  = Sys.getenv("TWITTER_ACCESS_TOKEN"),
+  access_secret = Sys.getenv("TWITTER_ACCESS_TOKEN_SECRET")
+)
 
 # Define column names
 col_names <- paste0("X", str_pad(as.character(1:50), width = 2, side = "left", pad = "0"))
@@ -12,6 +21,7 @@ df <- df %>%
   pivot_longer(everything()) %>% 
   drop_na(value)
 
+# Create random Tweet
 tweet <- df %>% 
   group_by(name) %>% 
   slice_sample(n = 1) %>% 
@@ -19,4 +29,9 @@ tweet <- df %>%
   pull(value) %>% 
   str_c(collapse = "")
 
-tweet
+
+# Post the tweet to Twitter
+post_tweet(
+  status = tweet,
+  token = londonmapbot_token
+)
